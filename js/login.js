998ebf19dc150ba461962e1891e6e760fc4ca8c8
extrafoto <130807385+extrafoto@users.noreply.github.com>
2025-07-20 14:40:16 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -2,19 +2,16 @@ document.addEventListener("DOMContentLoaded", function () {
   const modal = document.getElementById("loginModal");
   const senhaInput = document.getElementById("senha");
   const btnEntrar = document.getElementById("btnEntrar");
-  const senhaCorreta = "1234"; // você pode mudar aqui
+  const senhaCorreta = "1234"; // Altere aqui se quiser
 
-  modal.style.display = "flex"; // força o login aparecer
+  modal.style.display = "flex";
 
   btnEntrar.addEventListener("click", function () {
-    const senhaDigitada = senhaInput.value;
-
-    if (senhaDigitada === senhaCorreta) {
-      modal.style.display = "none"; // oculta o modal
-      document.body.classList.remove("bloqueado"); // libera o scroll, se tiver
-      carregarPainel(); // CHAMA a função para iniciar o painel
+    if (senhaInput.value === senhaCorreta) {
+      modal.style.display = "none";
+      carregarPainel(); // função que carrega o app
     } else {
-      alert("Senha incorreta!");
+      alert("Senha incorreta");
     }
   });
 });
